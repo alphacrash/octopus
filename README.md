58908ab34e8d83bcf8d59d7957dc1201d4d912d1
alphacrash <sudhansu.ace@gmail.com>
2021-12-14 00:55:00 +0530

    Checkpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+# To Implement
+
+1. Sorting
+2. Selecting
+3. Export to CSV, Excel
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,65 @@
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+} from "@mui/material";
+
+import {
+  transactionData,
+  transactionHeaders,
+} from "../service/TransactionData";
+
 function ReTable() {
   return (
-    <div>
-      <h1>ReTable</h1>
-    </div>
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {transactionHeaders.map((row, index) => {
+                  return (
+                    <TableCell
+                      key={row.id}
+                      align={row.numeric ? "right" : "left"}
+                      padding={row.disablePadding ? "none" : "normal"}
+                    >
+                      <TableSortLabel>{row.label}</TableSortLabel>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {transactionData.map((row, index) => {
+                return (
+                  <TableRow>
+                    <TableCell
+                      component="th"
+                      id={index}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 }
 
